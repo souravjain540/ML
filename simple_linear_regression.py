f4f697c45c6a2777b6cd5269b4633bf2e9fd28b1
Shritam Kumar Mund <shritamkumarmund.98@gmail.com>
2019-10-11 20:15:18 +0530

    Importing warnings to handle any kind of warnings
    It's always a good idea to ignore your warnings

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Importing warnings 
+import warnings
+warnings.filterwarnings('ignore')
+
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
